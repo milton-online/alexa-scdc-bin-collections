8b43c1f99b2ee093a07c5717596817ebf493cab3
Tim Cutts <tim@thecutts.org>
2020-12-21 22:08:50 +0000

    Separate out functions which search the collection lis
    so they can be tested properly, and write tests for them

---
+++ lambda/searchcollections.js
@@ -0,0 +1,36 @@
+/* Copyright 2020 Tim Cutts <tim@thecutts.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+const BinCollection = require("./bincollection.js")
+const SpeakableDate = require("./speakabledate.js")
+
+exports.getNextCollectionOfType = function (sessionData, binType) {
+  let r = sessionData.collections.find(function(item) {
+    let collectionDate = new SpeakableDate(item.date).getTime()
+    return item.roundTypes.indexOf(binType) !== -1 &&
+           collectionDate >= sessionData.midnightToday &&
+           collectionDate > sessionData.lastReportedBinTime
+  })
+  return r ? new BinCollection(r) : r
+}
+
+exports.getNextCollection = function(sessionData) {
+  let r = sessionData.collections.find(function(item) {
+    let collectionDate = new SpeakableDate(item.date).getTime()
+    return collectionDate >= sessionData.midnightToday &&
+           collectionDate > sessionData.lastReportedBinTime
+  })
+  return r ? new BinCollection(r) : r
+}
---
+++ test/test_searchcollections.js
@@ -0,0 +1,74 @@
+/* Copyright 2020 Tim Cutts <tim@thecutts.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+const should = require('should')
+const { getNextCollection, getNextCollectionOfType } = require("../lambda/searchcollections.js")
+const SpeakableDate = require ("../lambda/speakabledate.js")
+
+const today = new SpeakableDate().setToMidnight().getTime()
+const yesterday = Date.now() - 86400000
+
+datedAttributes = { collections:
+   [ { date: '1971-09-18T00:00:00Z',
+       roundTypes: ['DOMESTIC'],
+       slippedCollection: false,
+       ordinal: "zeroth"
+     },
+     { date: '2501-09-18T00:00:00Z',
+       roundTypes: ['DOMESTIC'],
+       slippedCollection: false,
+       ordinal: "first"
+     },
+     { date: '2501-09-24T00:00:00Z',
+       roundTypes: ['RECYCLE','ORGANIC'],
+       slippedCollection: true,
+       ordinal: "second"
+     } ,
+     { date: '2501-09-27T00:00:00Z',
+       roundTypes: ['DOMESTIC'],
+       slippedCollection: false,
+       ordinal: "third"
+   } ],
+  lastReportedBinTime: 0,
+  fetchedOnDate: yesterday,
+  currentBinType: null,
+  midnightToday: today
+}
+
+describe ('searchcollections', function() {
+    describe('getNextCollection()', function(){
+        it('returnsFirst', function() {
+            let c = getNextCollection(datedAttributes)
+            c.ordinal.should.equal("first")
+            datedAttributes.lastReportedBinTime = c.date.getTime()
+        })
+        it('returnsSecond', function() {
+            let c = getNextCollection(datedAttributes)
+            c.ordinal.should.equal("second")
+            datedAttributes.lastReportedBinTime = 0
+        })
+    })
+    describe('getNextCollectionOfType()', function() {
+        it('returnsFirst', function() {
+            let c = getNextCollectionOfType(datedAttributes, 'DOMESTIC')
+            c.ordinal.should.equal("first")
+            datedAttributes.lastReportedBinTime = c.date.getTime()
+        })
+        it('returnsThird', function() {
+            let c = getNextCollectionOfType(datedAttributes, 'DOMESTIC')
+            c.ordinal.should.equal("third")
+        })
+    })
+})
