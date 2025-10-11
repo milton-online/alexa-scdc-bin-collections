60c1591bf060ab601922b1a0725863ce2360c1a2
Tim Cutts <tim@thecutts.org>
2025-10-11 19:13:48 +0100

    simplified list of intents

--- lambda/index.js
+++ lambda/index.js
@@ -6,46 +6,32 @@
 "use strict";
 
 const Alexa = require("ask-sdk-core");
+const getPersistenceAdapter = require("./persistenceAdapter");
 
-const LaunchRequestHandler = require("./intents/LaunchRequestHandler");
-const NextColourBinIntentHandler = require("./intents/NextColourBinIntentHandler");
-const SetLogLevelIntentHandler = require("./intents/SetLogLevelIntentHandler");
-const MissedBinCollectionIntentHandler = require("./intents/MissedBinCollectionIntentHandler");
-const WhichBinTodayIntentHandler = require("./intents/WhichBinTodayIntentHandler");
-const GetFreshDataIntentHandler = require("./intents/GetFreshDataIntentHandler");
-const WhoPutTheBinsOutIntentHandler = require("./intents/WhoPutTheBinsOutIntentHandler");
-const CancelAndStopIntentHandler = require("./intents/CancelAndStopIntentHandler");
-const SessionEndedRequestHandler = require("./intents/SessionEndedRequestHandler");
-const HelpIntentHandler = require("./intents/HelpIntentHandler");
-const IntentReflectorHandler = require("./intents/IntentReflectorHandler");
-const YesIntentHandler = require("./intents/YesIntentHandler");
-const NoIntentHandler = require("./intents/NoIntentHandler");
+const handlers = [
+  "LaunchRequestHandler",
+  "YesIntentHandler",
+  "NoIntentHandler",
+  "NextColourBinIntentHandler",
+  "MissedBinCollectionIntentHandler",
+  "WhichBinTodayIntentHandler",
+  "GetFreshDataIntentHandler",
+  "SetLogLevelIntentHandler",
+  "WhoPutTheBinsOutIntentHandler",
+  "HelpIntentHandler",
+  "CancelAndStopIntentHandler",
+  "SessionEndedRequestHandler",
+  "IntentReflectorHandler",
+].map((name) => require(`./intents/${name}`));
 
 const LoadBinCollectionsInterceptor = require("./interceptors/LoadBinCollectionsInterceptor");
 const PersistenceSavingInterceptor = require("./interceptors/PersistenceSavingInterceptor");
-
 const ErrorHandler = require("./errors/ErrorHandler");
-
-const getPersistenceAdapter = require("./persistenceAdapter");
 
 exports.handler = Alexa.SkillBuilders.custom()
   .withPersistenceAdapter(getPersistenceAdapter())
   .withApiClient(new Alexa.DefaultApiClient())
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    YesIntentHandler,
-    NoIntentHandler,
-    NextColourBinIntentHandler,
-    MissedBinCollectionIntentHandler,
-    WhichBinTodayIntentHandler,
-    GetFreshDataIntentHandler,
-    SetLogLevelIntentHandler,
-    WhoPutTheBinsOutIntentHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler,
-    IntentReflectorHandler
-  )
+  .addRequestHandlers(...handlers)
   .addRequestInterceptors(LoadBinCollectionsInterceptor)
   .addResponseInterceptors(PersistenceSavingInterceptor)
   .addErrorHandlers(ErrorHandler)
